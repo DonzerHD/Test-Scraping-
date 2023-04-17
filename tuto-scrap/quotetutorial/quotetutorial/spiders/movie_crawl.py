43fa4cb50e37ca000575441bf3b108549d33f538
DonzerHD <thomas.lemay59@outlook.fr>
2023-04-17 22:02:11 +0200

    ajout genre

--- tuto-scrap/quotetutorial/quotetutorial/spiders/movie_crawl.py
+++ tuto-scrap/quotetutorial/quotetutorial/spiders/movie_crawl.py
@@ -1,13 +1,15 @@
-import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from ..items import MovieItem
+import logging
+from termcolor import colored
 
 class IMDbTop250Spider(CrawlSpider):
     name = 'imdb_top_250_spider'
     allowed_domains = ['imdb.com']
     start_urls = ['https://www.imdb.com/chart/top/']
-    
+    movie_count = 0
+
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
     }
@@ -18,10 +20,15 @@ class IMDbTop250Spider(CrawlSpider):
 
     def parse_movie(self, response):
         title = response.css("h1[data-testid='hero__pageTitle'] span::text").get().strip()
-        genre = response.css("li.ipc-metadata-list__item[data-testid='storyline-genres'] a::text").get()
-        
+        genre = response.css('a.ipc-chip--on-baseAlt span.ipc-chip__text::text').getall()
+
+        # Incrémentation du compteur et affichage d'un message d'information coloré
+        self.movie_count += 1
+        log_message = colored(f"Film {self.movie_count}: {title}", 'cyan')
+        logging.info(log_message)
+
         movie_item = MovieItem()
         movie_item['title'] = title
         movie_item['genre'] = genre
-        
+
         yield movie_item
